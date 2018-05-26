4030fbcc87035c5e5731f6043ec5666ac640936b
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-26 16:33:28 -0400

    Fix port number

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 
 models.sequelize.sync().then(() => {
