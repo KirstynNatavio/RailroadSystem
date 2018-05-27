c4071477fedf055c12a3444aa7f7aea261a2db7d
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-27 00:38:31 -0400

    Fix station schema

--- models/station.js
+++ models/station.js
@@ -28,6 +28,9 @@ module.exports = (sequelize, DataTypes) => {
 		models.STATION.belongsTo(models.TRIP, {as: 'DESTINATION'});
 
 
+		models.STATION.belongsTo(models.SEGMENT, {foreignKey: 'STATION_ID', targetKey: 'NORTH_END'});
+		models.STATION.belongsTo(models.SEGMENT, {foreignKey: 'STATION_ID', targetKey: 'SOUTH_END'});
+
 		models.STATION.belongsToMany(models.TRAIN, {through: models.STOPS_AT })	
 
 	};
