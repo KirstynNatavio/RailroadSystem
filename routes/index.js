990804d8301b33f8c9d8d25e4fdeaea8842a45d2
Patrick <pvanny1124@gmail.com>
2018-05-27 05:37:52 +0000

    add format phone code

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
 
+/* CREATE */
+router.post('/', function(req, res){
+    
+                   var firstName    = req.body.firstName;
+                   var lastName     = req.body.lastName;
+                   var email        = req.body.email;
+                   var phone        = req.body.phone;
+                   var origin       = req.body.origin;
+                   var destination  = req.body.destination;
+                   var date         = req.body.date;
+                   var numberOfPets = req.body.pets;
+                   var adultFare    = req.body.adultFare;
+                   var childFare    = req.body.childFare;
+                   var elderlyFare  = req.body.elderlyFare;
+                   
+                
+                   
+});
 
 module.exports = router;
