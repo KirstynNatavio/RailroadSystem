3aa1bd4bfd44417182938281e6068a9776f34b34
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-27 04:11:47 -0400

    Test FK

--- models/station.js
+++ models/station.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
 
 
 		models.STATION.belongsTo(models.TRIP, {sourceKey: 'STATION_ID', targetKey: 'ORIGIN'});
-		models.STATION.belongsTo(models.TRIP, {sourceKey: 'STATION_ID', targetKey: 'DESTINATION'});
+		// models.STATION.belongsTo(models.TRIP, {sourceKey: 'STATION_ID', targetKey: 'DESTINATION'});
 
 
 		// models.STATION.belongsTo(models.SEGMENT, {foreignKey: 'STATION_ID', targetKey: 'NORTH_END'});
