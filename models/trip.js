24701eb84b070122cc0fa97e6ad5ecc22aca5a7d
Patrick <pvanny1124@gmail.com>
2018-05-27 02:49:48 +0000

    add stuff

--- models/trip.js
+++ models/trip.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
 	});
 
 	TRIP.associate = function(models) {
-		models.TRIP.belongsToMany(models.TRAIN, {as: "TRAIN_ID"});
+		models.TRIP.belongsToMany(models.TRAIN, {through: "train_trip"});
 		models.TRIP.belongsTo(models.PASSENGER);
 		models.TRIP.belongsTo(models.RESERVATION);
 		models.TRIP.hasMany(models.STATION);
