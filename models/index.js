81a5a2c5038ab7c99618cd6ad5cc604f64272556
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-28 07:49:08 -0400

    Change host sequelize'

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const env = process.env.NODE_ENV || 'development';
 const db = {};
 
 const sequelize = new Sequelize('S18336PRRteam1', 'user', 'password', {
-	host: '96.246.237.159',
+	host: 'localhost',
 	dialect: 'mysql',
 	port: 3306
 });	
