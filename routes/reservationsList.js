2c9d71a5a8c3c681ba2093a73e3883287ef9b315
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-28 09:19:41 -0400

    Fix form

--- routes/reservationsList.js
+++ routes/reservationsList.js
@@ -2,9 +2,30 @@ const models = require("../models");
 const express = require('express');
 const sequelize = require('sequelize');
 var router = express.Router();
-
+var passengerObj;
+var reservationObj;
 router.post('/', function(req, res, next) {
-	res.send(req.body.email);
+	models.PASSENGER.findAll({
+		where:{
+			EMAIL: req.body.email
+		}
+	}).then((passenger) => {
+		passengerObj = passenger;
+		models.RESERVATIONS.findOne({
+			where: {
+				RESERVATION_ID: req.body.reservation
+			}
+		}).then((reservation) => {
+			reservationObj = reservation;
+			models.TRIP.findAll({
+				where: {
+					RESERVATION_ID: reservation.RESERVATION_ID
+				}
+			}).then((allTrips) => {
+				res.render('/', {passengerObj, reservationObj, allTrips})
+			})
+		})
+	})
 	// models.RESERVATIONS.findAll({
 	// }).then(())
  //  res.render('reservationsList');
