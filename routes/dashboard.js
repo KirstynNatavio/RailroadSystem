b9f5fc70be150428a25ea5214d85b1f8e157f809
Patrick <pvanny1124@gmail.com>
2018-05-28 13:51:59 +0000

    add get_price

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -34,8 +34,58 @@ router.post('/', function(req, res){
                   currentDate         = Date.now();
                   
           var passengerId             = null,
-                  trainId             = null;
-                
+                  trainId             = null,
+                  age                 = null,
+                  time                = req.body.time;
+                  
+                  
+                                   
+                       //Set veteran/military variables to bits for yes/no
+                       //These bits will be used to compute the GET_PRICE procedure in the database
+                      (disabled.toLowerCase() == 'yes') ? disabled = 1 : disabled = 0;
+                      (veteran.toLowerCase() == 'yes') ? veteran = 1 : veteran = 0;
+                      if(adultFare.toLowerCase() == 'adult') age = 1;
+                      if(childFare.toLowerCase() == 'child') age = 0;
+                      if(elderlyFare.toLowerCase() == 'elderlyFare') age = 2;
+                       
+                       /* GET_PRICE parameters: */
+                       
+                       /*  IN ORIGIN INT(11), 
+                           IN DEST INT(11), 
+                           IN DIS_BIN BIT, 
+                           IN MILIT_BIN BIT, 
+                           IN AGE INT(1), 
+                           IN PET_NUM INT, 
+                           IN TRIP_DATE DATE, 
+                           IN RES_DATE DATE, 
+                           OUT PRICE DOUBLE(6,2))
+                       */
+                  
+                  
+            var replacements = [
+                             origin,
+                             destination,
+                             disabled,
+                             veteran,
+                             age,
+                             numberOfPets,
+                             currentDate,
+                             reservationDate
+               ];
+              
+               //Call GET_PRICE with sequelize.query and pass in relevant info
+             sequelize.query('call GET_PRICE(?, ?, ?, ?, ?, ?, ?, ?, @PRICE_OF_FARE);', { 
+                     replacements: replacements, 
+                     type: sequelize.QueryTypes.SELECT 
+            
+             }).then(projects => {
+                 console.log(projects);
+               });
+               
+            sequelize.query('select @PRICE_OF_FARE').then(price => {
+                  console.log(price);
+            })
+      
                 models.PASSENGER.create({
                     FIRST_NAME: firstName,
                     LAST_NAME: lastName,
@@ -61,7 +111,9 @@ router.post('/', function(req, res){
                                    PASSENGER_ID:     passengerId,
                                    ORIGIN:           origin,
                                    DESTINATION:      destination,
-                                   TRIP_DATE:        reservationDate
+                                   TRIP_DATE:        reservationDate,
+                                   TRIP_TIME:        time,
+                                   PRICE:            price
                              });
                             
                           });
@@ -92,24 +144,7 @@ module.exports = router;
 //                             res.send(JSON.stringify(projects));
 //                     });
                     
-//             var replacements = [
-//                             origin,
-//                             destination,
-//                             disabled,
-//                             veteran,
-//                             numberOfPets,
-//                             currentDate,
-//                             reservationDate
-//               ];
-              
-//               //Call GET_PRICE with sequelize.query and pass in relevant info
-//                       sequelize.query('call GET_PRICE(?, ?, ?, ?, ?, ?, ?, ?, @PRICE_OF_FARE);', { 
-//                               replacements: replacements, 
-//                               type: sequelize.QueryTypes.SELECT 
-                      
-//                       }).then(projects => {
-//                           console.log(projects);
-//                         });
+
                          
 //                         //ORIGIN
 //                         //DEST
@@ -131,25 +166,7 @@ module.exports = router;
 /* CREATE */
 
                  
-                       
-//                       //Set veteran/military variables to bits for yes/no
-//                       //These bits will be used to compute the GET_PRICE procedure in the database
-//                      (disabled.toLowerCase() == 'yes') ? disabled = 1 : disabled = 0;
-//                      (veteran.toLowerCase() == 'yes') ? veteran = 1 : veteran = 0;
-                
-                       
-//                       /* GET_PRICE parameters: */
-                       
-//                       /*  IN ORIGIN INT(11), 
-//                           IN DEST INT(11), 
-//                           IN DIS_BIN BIT, 
-//                           IN MILIT_BIN BIT, 
-//                           IN AGE INT(1), 
-//                           IN PET_NUM INT, 
-//                           IN TRIP_DATE DATE, 
-//                           IN RES_DATE DATE, 
-//                           OUT PRICE DOUBLE(6,2))
-//                       */
+
                        
 //                       var replacements = [
 //                             origin,
