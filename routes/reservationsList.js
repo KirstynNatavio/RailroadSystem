d2cd485680b7adb8ce63713106abd29f6fb270a6
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-28 08:53:06 -0400

    Change routes

--- routes/reservationsList.js
+++ routes/reservationsList.js
@@ -3,8 +3,8 @@ const express = require('express');
 const sequelize = require('sequelize');
 var router = express.Router();
 
-router.get('/:email', function(req, res, next) {
-	res.send(req.params.email);
+router.get('/', function(req, res, next) {
+	res.send(req.body.email);
 	// models.RESERVATIONS.findAll({
 	// }).then(())
  //  res.render('reservationsList');
