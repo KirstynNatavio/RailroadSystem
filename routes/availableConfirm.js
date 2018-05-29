661960d17ff554b3b38cd20819a7e7ba50f2540b
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-29 06:45:00 -0700

    Fix something

--- routes/availableConfirm.js
+++ routes/availableConfirm.js
@@ -3,7 +3,8 @@ const express = require('express');
       
 var router = express.Router();
 router.post('/', function(req, res, next) {
-  res.render('availableConfirm');    
+	res.send(req.body.available);
+  //res.render('availableConfirm');    
         
 });
 
