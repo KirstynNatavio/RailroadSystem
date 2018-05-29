b753031e9f0bec611fa2e0bd72e74419a17cd18b
KirstynNatavio <kirstynatavio@hotmail.com>
2018-05-29 13:36:22 -0700

    Fix stuff

--- routes/reservationsList.js
+++ routes/reservationsList.js
@@ -9,21 +9,30 @@ var tripObj;
 var originObj;
 var destinationObj;
 var email;
+var valid;
 
 router.post('/', function(req, res, next) {
-	
+	valid = true;
 	email = req.body.email;
 	models.PASSENGER.findOne({
 		where:{
 			EMAIL: email
 		}
 	}).then((passenger) => {
+		if (passenger == null) {
+			valid = false;
+			res.render('reservationsList', {passengerObj, reservationObj, tripObj, originObj, destinationObj, valid})
+		}
 		passengerObj = passenger;
 		models.RESERVATION.findOne({
 			where: {
 				RESERVATION_ID: parseInt(req.body.reservation)
 			}
 		}).then((reservation) => {
+			if (reservation == null) {
+				valid = false;
+				res.render('reservationsList', {passengerObj, reservationObj, tripObj, originObj, destinationObj, valid})
+			}
 			reservationObj = reservation;
 			models.TRIP.findOne({
 				where: {
@@ -52,7 +61,7 @@ router.post('/', function(req, res, next) {
 							}
         					module.exports = options;
         // 						if(email != null && reservationId != null){
-								// 		res.render('reservationsList', {passengerObj, reservationObj, tripObj, originObj, destinationObj})
+							res.render('reservationsList', {passengerObj, reservationObj, tripObj, originObj, destinationObj, valid})
 								// } else {
 								// 	//req.flash("error", "There is no reservation on file with the email");
 								// 	res.render('reservations');
